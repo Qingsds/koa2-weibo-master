0d147508bc690860b129ed122415465eb9bb4400
Qingsds <827824175@qq.com>
2022-04-21 14:24:22 +0800

    fix: 判断用户是否已登录

--- src/routes/view/user.js
+++ src/routes/view/user.js
@@ -5,12 +5,31 @@
 
 const router = require("koa-router")()
 
+/**
+ * 获取用户信息
+ * @param {Object} ctx 上下文
+ * @returns  data
+ */
+function getUserInfo(ctx) {
+    const data = {
+        isLogin: false,
+    }
+    const userInfo = ctx.session.userInfo
+    if (userInfo) {
+        Object.assign(data, {
+            isLogin: true,
+            userName: userInfo.userName,
+        })
+    }
+    return data
+}
+
 router.get("/login", async (ctx, next) => {
-    await ctx.render("login", {})
+    await ctx.render("login", getUserInfo(ctx))
 })
 
 router.get("/register", async (ctx, next) => {
-    await ctx.render("register", {})
+    await ctx.render("register", getUserInfo(ctx))
 })
 
 module.exports = router
