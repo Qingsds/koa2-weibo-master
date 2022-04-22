b12eb99a60c7a1e0c6ed0afb20d96a4bbad55a41
Qingsds <827824175@qq.com>
2022-04-22 10:11:54 +0800

    feat: 添加设置页面

--- src/routes/view/index.js
+++ src/routes/view/index.js
@@ -3,7 +3,9 @@ const { loginRedirect } = require("../../middlewares/loginChecks")
 const router = require("koa-router")()
 
 router.get("/", loginRedirect, async (ctx, next) => {
-    await ctx.render("index", {})
+    await ctx.render("index", {
+        title: '首页 test'
+    })
 })
 
 module.exports = router
--- src/routes/view/user.js
+++ src/routes/view/user.js
@@ -4,6 +4,7 @@
  */
 
 const router = require("koa-router")()
+const { loginRedirect } = require("../../middlewares/loginChecks")
 
 /**
  * 获取用户信息
@@ -30,6 +31,10 @@ router.get("/login", async (ctx, next) => {
 
 router.get("/register", async (ctx, next) => {
     await ctx.render("register", getUserInfo(ctx))
+})
+
+router.get("/setting", loginRedirect, async (ctx, next) => {
+    await ctx.render("setting", ctx.session.userInfo)
 })
 
 module.exports = router
